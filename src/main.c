f4fbabb9800de24df695f7f489e8d291fd549914
pwq <1066784440@qq.com>
2025-01-19 22:22:40 +0800

    调了一下参数，使亮度差别明显。 将PWM相关参数改为宏定义，相同占空比的位以后缀指明，如DutyCycle0123就是指P20,P21,P22,P23的占空比一致，将它门置位或复位的方式在所赋的值里体现，如：1111 0000b=P2 & ~0x0f | 0x0f; 有两个问题需要注意，计数器的值是不是遍历了0~resolution？分辨率太细，对于51单片机可能会导致总的周期时间太长，使得亮度不稳，出现闪烁。也就是说频率越低，周期越长，PWM的方式就容易导致闪烁。

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <STC89C5XRC.h>
 #include <INTRINS.H>
 
+// 占空比分辨率控制 PWM
+// 周期长度 = Frequency*Resolution = 1ms = 1000hz
+#define FREQUENCY Delay10us()
+// 占空比，取值范围为0~resolution-1，0=0%, resolution=100%
+#define DUTYCYCLE0123 5
+#define DUTYCYCLE4567 90
+// 分辨率 = 1/resolution
+#define RESOLUTION 100
+
 typedef unsigned char uint8_t;
 typedef unsigned int uint16_t;
 typedef unsigned long uint32_t;
@@ -9,45 +18,36 @@ typedef signed char int8_t;
 typedef signed int int16_t;
 typedef signed long int32_t;
 
-// delay for num half seconds.
-void Delay100us(void);
+// delay 100us
+void Delay10us(void);
 
 // 要实现不同占空比信号来调节led亮度
 void main()
 {
     uint8_t i = 0;
     while (1) {
-        // 跟8取余就是设一个周期长8*100ms，同样一个周期被划为8段，设置一个灯亮的段数，就是在设置占空比。
-        // 当取余为0时，就是第一段内，但是在第一段的开头还是结尾，看延时函数在赋值前还是赋值后，在后，就是在第一段开头
-        if (i % 8 == 0) {
-            // 赋初值应考虑到下方占空比大小与亮度大小的对应关系，习惯上觉得，
-            // 下方if里的数字越大，应该越亮，所以初值应该为全亮，然后在逐个熄灭
-            P2 = 0x00;
-        }
-        // 这里所有刻度都列出来了，实际使用只需给出所需刻度值即可
-        // 也许应该定义一个宏函数，来置位和复位。算了，可能情况太多，不好定义。
-        // 置位操作：|1，复位操作：&0，由于初值为全0，所以后续只需置位即可。
+        // 这里不用取余，用跟基本的减法运算，将变化范围限制在 0~RESOLUTION
+        // 赋初值应考虑到下方占空比大小与亮度大小的对应关系，习惯上觉得，
+        // 下方if里的数字越大，应该越亮，所以初值应该为全亮，然后在逐个熄灭
+        // 实验证明，就算是两个相邻的if语句，之间的时间都足以让led有肉眼可见的亮度。
+        // 所以0%的占空比只能在赋初值出就直接设置。
+        if (i == 0) P2 = 0x00;
         // 多位一同赋值建议使用格式：var = var & ~0x09 | 0x09; 这样可以同时设置0和1，
         // 之所以要在复位那里用取反号，是因为转化为16进制好计算，如1011可以写成~0100，即~0x4
-        if (i % 8 == 1) P2 = P2 & ~0x0f | 0x0f;
-        //        if (i % 8 == 2) P2 = P2 & ~0x0f | 0x04;
-        //        if (i % 8 == 3) P2 = P2 & ~0x0f | 0x08;
-        //        if (i % 8 == 4) P2 = P2 & ~0x0f | 0x10;
-        //        if (i % 8 == 5) P2 = P2 & ~0x0f | 0x20;
-        //        if (i % 8 == 6) P2 = P2 & ~0x0f | 0x40;
-        if (i % 8 == 7) P2 = P2 & ~0xf0 | 0xf0;
-        // 保证每轮取余的结果相同，否则将240改为243，就有可能出现最后一个周期只有三段，占空比就乱了
-        if (i == 240) i = 0;
-        Delay100us();
+        if (i == DUTYCYCLE0123) P2 = P2 & ~0x0f | 0x0f;
+        if (i == DUTYCYCLE4567) P2 = P2 & ~0xf0 | 0xf0;
+        // 计数完一个周期，就恢复初值
+        FREQUENCY;
         i++;
+        // 初值维护语句必须在 i++ 下面，否则会导致 i 永不可能为 0，所有灯闪一下后，全灭
+        if (i == RESOLUTION) i = 0;
     };
 }
 
-void Delay100us(void) //@11.0592MHz
+void Delay10us(void) //@11.0592MHz
 {
     unsigned char data i;
 
-    _nop_();
-    i = 43;
+    i = 2;
     while (--i);
 }
