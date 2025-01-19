b4f5c26de5ea384ffbbce5e463f20c23ea8126f5
pwq <1066784440@qq.com>
2025-01-19 23:13:18 +0800

    数码管静态显示。 登记了数码管的段码表（小数点默认不亮），弄清了数字编号顺序。

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <STC89C5XRC.h>
 
+#define DIGITALTUBESBIT(n)  P2 = P2 & ~0x1c | (n << 2)
+#define DIGITALTUBESSEGMENT P0
+
 typedef unsigned char uint8_t;
 typedef unsigned int uint16_t;
 typedef unsigned long uint32_t;
@@ -8,11 +11,14 @@ typedef signed char int8_t;
 typedef signed int int16_t;
 typedef signed long int32_t;
 
+// 所有小数点均不亮
 uint8_t codeTable[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
 
 void main()
 {
-    while (1) {
-        // TODO
-    }
+    // 选中最右边的数字，数字编号从右至左 0~7
+    DIGITALTUBESBIT(0x03);
+    // 使其显示为 0
+    DIGITALTUBESSEGMENT = codeTable[8];
+    while (1) {}
 }
