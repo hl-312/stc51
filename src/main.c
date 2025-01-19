81fde3c597a4e1504fc06d03c826fc97dc307e83
pwq <1066784440@qq.com>
2025-01-19 22:31:19 +0800

    初次提交。 只添加了数码管的段码表。

--- src/main.c
+++ src/main.c
@@ -8,6 +8,8 @@ typedef signed char int8_t;
 typedef signed int int16_t;
 typedef signed long int32_t;
 
+uint8_t codeTable[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+
 void main()
 {
     while (1) {
