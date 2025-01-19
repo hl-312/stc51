e6d3ab1d0b76bfe64bd289563a8ff7cd8613b769
pwq <1066784440@qq.com>
2025-01-19 17:47:45 +0800

    使用了 isp 来生成软件延时函数。 延时的数据计算涉及时间周期问题。 单片机的时间分出三种周期：时钟周期，机器周期，指令周期， 分别代表时钟的最小时间单位， 机器动作的最小时间单位， 执行一条指令的最小时间单位。
    具体计算要参考汇编代码和指令表（包含了指令与周期的对应关系）。

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <STC89C5XRC.h>
+#include <INTRINS.H>
 
 typedef unsigned char uint8_t;
 typedef unsigned int uint16_t;
@@ -8,18 +9,37 @@ typedef signed char int8_t;
 typedef signed int int16_t;
 typedef signed long int32_t;
 
+void Delay500ms(void); //@11.0592MHz
+
 void main()
 {
-    uint16_t i = 0;
-    uint8_t j  = 0x01;
-    P2         = 0xff;
+    uint8_t set = 0x01;
+    P2          = 0xff;
     while (1) {
-        i++;
-        if (i == 33000) {
-            P2 = 0xff ^ j;
-            j <<= 1;
-            if (j == 0) j = 0x01;
-            i = 0;
-        }
+        P2 = ~set;
+        /*
+        P2 = set ^ P2;  这种写法也可以，0^0=0,1^0=1,0^1=1,1^1=0,
+        P2默认关，值全1，则与掩码异或后，特定位相同被置0点亮，其余位不同同仍为1关闭，
+        而已被点亮的位与下一位掩码均为0，相同置1，关闭，效果与取反相同。
+        当然现在看来不如取反直接，容易理解。
+        */
+        set <<= 1;
+        if (set == 0) set = 0x01;
+        Delay500ms();
     };
+}
+
+void Delay500ms(void) //@11.0592MHz
+{
+    unsigned char data i, j, k;
+
+    _nop_();
+    i = 4;
+    j = 129;
+    k = 119;
+    do {
+        do {
+            while (--k);
+        } while (--j);
+    } while (--i);
 }
